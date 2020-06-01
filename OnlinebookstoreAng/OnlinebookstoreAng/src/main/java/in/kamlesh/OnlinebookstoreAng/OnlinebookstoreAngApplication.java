5f9722bcc1af0af775838a42da0a52310382bb14
kamlesh <kamlesh.merugu30@gmail.com>
2020-06-01 11:05:52 +0800

    Initial Commit.Created spring boot and configure the application properties

---
+++ OnlinebookstoreAng/OnlinebookstoreAng/src/main/java/in/kamlesh/OnlinebookstoreAng/OnlinebookstoreAngApplication.java
@@ -0,0 +1,13 @@
+package in.kamlesh.OnlinebookstoreAng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinebookstoreAngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinebookstoreAngApplication.class, args);
+	}
+
+}
